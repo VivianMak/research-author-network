1548a757495d2cf517dd5cb7f71b0a7354083ab7
VivianMak <vivianmak.30@gmail.com>
2025-12-04 09:11:27 -0500

    network structure, added main

--- src/network.py
+++ src/network.py
@@ -1,13 +1,84 @@
 # Node class
 
-class Network:
+from utils import Node
+from typing import List
+
+class NetworkGraph:
     """Create a network"""
+
     def __init__(self):
+        """Initialize an instance of the Network Graph."""
+
+        self.node_list = []
+
+
+    def add_node(self, 
+                 author_name,
+                 author_id,
+                 collab_edges,
+                 citation_count,
+                 paper_count):
+        """
+        Make a new node with a new author
+        
+        Args:
+            author_name: Description
+            author_id: Description
+            collab_edges: Description
+            citation_count: Description
+            paper_count: Description
+        """
+        
+        # Check if node already exists
+        if author_id in self.node_list:
+            self.edit_node(author_name,
+                            author_id,
+                            collab_edges,
+                            citation_count,
+                            paper_count)
+            
+
+        new_node = Node(
+            author_name=author_name, 
+            author_id=author_id, 
+            collab_edges=collab_edges,
+            citation_count=citation_count,
+            paper_count=paper_count)
+        
+        self.node_list.append(new_node)
+
+    def edit_node(self, author_name,
+                        author_id,
+                        collab_edges,
+                        citation_count,
+                        paper_count):
+        """"""
         pass
 
-    def add_node(self):
-        pass
+    def node_lookup(self):
+        """Turn node list into a dictionary for quick key lookup."""
+
+         # Create a dictionary for quick lookups
+        data_dict = {item.key: item for item in self.node_list}
 
     def add_edge(self):
         pass
+
+    def calculate_weight(self):
+        """Calculate the collaborations to populate the collaboration edges of a node with author_id and num_of_collabs."""
+
+
+    
+
+    def connected_authors(self, node:Node) -> List[Node]:
+        """
+        Return the authors connected to the author (node).
+
+        Args:
+            node: a node representing the author to query neighbors
+
+        Return:
+            a list of neighbor nodes
+        """
+
     
