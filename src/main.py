ee5a393c5f03dd05996f0771967b9789f6c23e6d
VivianMak <vivianmak.30@gmail.com>
2025-12-07 15:05:54 -0500

    redid network class with adj matrix

--- src/main.py
+++ src/main.py
@@ -4,6 +4,14 @@ from network import NetworkGraph
 def main():
     
     graph = NetworkGraph()
+    author = "vp"
+    collabs = ["bm", "rw"]
+    if not graph.check_author(author):
+        graph.add_new_author(author)
+
+    graph.add_author_collab(author, collabs)
+
+    graph.get_collabs()
 
 
 
--- src/network.py
+++ src/network.py
@@ -1,7 +1,5 @@
-# Node class
-
-from utils import Node
 from typing import List
+import pandas as pd
 
 class NetworkGraph:
     """Create a network"""
@@ -9,79 +7,77 @@ class NetworkGraph:
     def __init__(self):
         """Initialize an instance of the Network Graph."""
 
-        self.node_list = []
+        # Keeps track of matrix headers
+        self.author_ids = []
+
+        # Dictionary "author_id": (name,)
+        self.author_info = {}
+
+        self.collabs_mat = []
 
 
-    def add_node(self, 
-                 author_name,
-                 author_id,
-                 collab_edges,
-                 citation_count,
-                 paper_count):
+    def add_new_author(self, author_id):
         """
-        Make a new node with a new author
+        Create a new row/col for an author and populate collaborations
         
         Args:
-            author_name: Description
-            author_id: Description
-            collab_edges: Description
-            citation_count: Description
-            paper_count: Description
+            author_id: (string) the id of corresponding author
         """
-        
-        # Check if node already exists
-        if self.node_lookup(author_id):
-            self.edit_node(author_name,
-                           author_id,
-                           collab_edges)
-            
+        print("----ADDING NEW AUTHOR-----")
+        # Add new author to list
+        self.author_ids.append(author_id)
 
-        new_node = Node(
-            author_name=author_name, 
-            author_id=author_id, 
-            collab_edges=collab_edges)
-        
-        self.node_list.append(new_node)
+        n = len(self.author_ids)
 
-    def edit_node(self, author_name,
-                        author_id,
-                        collab_edges):
-        """"""
-        pass
+        # Populate row and col with zeros
+        self.collabs_mat.append([0]*(n))
 
-    def node_lookup(self, author_id):
-        """Turn node list into a dictionary for quick key lookup."""
+        for i in range(n-1):
+            self.collabs_mat[i].append(0)
 
-         # Create a dictionary for quick lookups
-         idx, node = next(((i,node) for i,node in self.node_list if node.author_id == author_id), None)
-
-        return idx, node
-
-
-
-    def add_edge(self, node:Node):
-        """Update the edges of an author."""
-
-        # Check if collaboration already exists
-        if node not in self.collab_edges:
-            node.collab_edges.append(node)
-
-
-    def calculate_weight(self):
-        """Calculate the collaborations to populate the collaboration edges of a node with author_id and num_of_collabs."""
-
-
-    
-
-    def connected_authors(self, node:Node) -> List[Node]:
-        """
-        Return the authors connected to the author (node).
-
+    def add_author_collab(self, author_id, collabs):
+        """For an existing author, adjust the collaborations.
         Args:
-            node: a node representing the author to query neighbors
+            author_id: (string) the id of corresponding author.
+            collabs: (list) a list of collaborated authors.
+        """
 
+        idx = self.author_ids.index(author_id)
+
+        for c in collabs:
+
+            # Add row and col for new authors
+            if not self.check_author(c):
+                self.add_new_author(author_id=c)
+
+                # New author is known at last index
+                self.collabs_mat[idx][-1] += 1
+                self.collabs_mat[-1][idx] += 1
+                
+            else:
+                # Find seen author index
+                i = self.author_ids.index(c)
+                self.collabs_mat[idx][i] += 1
+                self.collabs_mat[i][idx] += 1
+
+
+    def check_author(self, author_id) -> bool:
+        """Check if author exists.
+        Args:
+            author_id: (string) the id of corresponding author.
         Return:
-            a list of neighbor nodes
+            true if author has been seen
         """
 
-    
+        if author_id in self.author_ids:
+            return True
+        return False
+
+    def get_collabs(self):
+        """Format the adjacency matrix as a dataframe"""
+
+        print("Showing final collboration adjacency matrix...")
+
+        df = pd.DataFrame(self.collabs_mat, columns=self.author_ids, index=self.author_ids)
+        print(df)
+
